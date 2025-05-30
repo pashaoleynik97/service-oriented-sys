f65b4bafe4101b7ea134b54eca6503d078531c0e
pashaoleynik97 <mathahakar1997@gmail.com>
2025-05-30 15:16:36 +0300

    LR6

--- src/main/kotlin/com/oliinyk/lab1/Lab1Application.kt
+++ src/main/kotlin/com/oliinyk/lab1/Lab1Application.kt
@@ -1,5 +1,6 @@
 package com.oliinyk.lab1
 
+import jakarta.annotation.PostConstruct
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
@@ -8,4 +9,9 @@ class Lab1Application
 
 fun main(args: Array<String>) {
 	runApplication<Lab1Application>(*args)
+}
+
+@PostConstruct
+fun printZipkinConfig() {
+	println("Zipkin config: ${System.getenv("SPRING_ZIPKIN_BASE_URL")}")
 }
